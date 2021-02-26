84195620458179f33f2429a4cf50582d5609cea2
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-02-26 18:27:55 +0600

    header and load data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './component/Header/Header';
+import Product from './component/Product/Product';
+import Shop from './component/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
+      <Product></Product>
     </div>
   );
 }
